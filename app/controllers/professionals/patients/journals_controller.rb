4aab47c10759d0d934d297fdefcdc477d92efc5b
Saulo Arruda <sauloarruda@gmail.com>
2026-03-02 13:41:22 -0300

    feat: implement journals controller with show action and breadcrumb navigation

---
+++ app/controllers/professionals/patients/journals_controller.rb
@@ -0,0 +1,31 @@
+module Professionals
+  module Patients
+    class JournalsController < Professionals::Patients::BaseController
+      skip_before_action :authorize_owner!
+
+      def show
+        date = parse_date_param(params[:date]) || Date.current
+
+        @journal = @patient.journals.find_by(date: date) || @patient.journals.build(date: date)
+        @meals = @journal.persisted? ? @journal.meals.order(:created_at) : Meal.none
+        @exercises = @journal.persisted? ? @journal.exercises.order(:created_at) : Exercise.none
+
+        render "journals/show"
+      end
+
+      def today
+        redirect_to journal_professional_patient_path(@patient, date: Date.current.iso8601)
+      end
+
+      private
+
+      def parse_date_param(raw_date)
+        return nil if raw_date.blank?
+
+        Date.iso8601(raw_date)
+      rescue ArgumentError
+        nil
+      end
+    end
+  end
+end
